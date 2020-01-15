1882979318ccb13df9dc824c59f785779e9cc686
Rafael Carvalho <rafaelcarvalhocs2016@gmail.com>
2020-01-15 11:03:29 -0300

    Excluir funcionarios

---
+++ src/exercicios/Exercicio1.java
@@ -0,0 +1,22 @@
+package exercicios;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        double[] notas = new double[4];
+
+        notas[0] = 0.23;
+        notas[1] = 3.45;
+        notas[2] = 7.90;
+        notas[3] = 5.90;
+
+        double media = (notas[0] + notas[1] + notas[2] + notas[3])/4;
+
+        System.out.println(media);
+
+        if(media <5){
+            System.out.println("reprovado");
+            System.out.println("Suas notas foram");
+        }
+    }
+
+}
